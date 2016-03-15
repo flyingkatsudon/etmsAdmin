7b08c2cc752e7ecb6a3ace7ee2d525b8dc4674dd
MuzEye <muzeye@gmail.com>
2016-03-15 10:06:50 +0900

    .

--- src/main/resources/static/require.config.js
+++ src/main/resources/static/require.config.js
@@ -3,8 +3,8 @@ require.config({
         layout: {deps: ['jquery']}
     },
     paths: {
-        jquery: 'bower_components/jquery/dist/jquery.min',
-        layout: 'bower_components/jquery-ui-layout/source/stable/jquery.layout_and_plugins.min'
+        jquery: 'components/jquery/dist/jquery.min',
+        layout: 'components/jquery-ui-layout/source/stable/jquery.layout_and_plugins.min'
     },
     map: {
         '*': {'css': 'webjars/require-css/0.1.8/css.min'}
