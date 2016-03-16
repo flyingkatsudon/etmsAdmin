9e10bb4b38af4be8c307a1be5c2aad75147d3e62
MuzEye <muzeye@gmail.com>
2016-03-16 19:03:47 +0900

    .

---
+++ src/main/resources/static/js/app/grid/grid-status-major.js
@@ -0,0 +1,46 @@
+/**
+ *
+ */
+define(function (require) {
+    "use strict";
+    var GridBase = require('./grid-base');
+
+    return GridBase.extend({
+        initialize: function (options) {
+            var colModel = [
+                {name: 'logSeq'},
+                {name: 'logDttm'},
+                {name: 'exmSeq', hidden: true},
+                {name: 'exmDt'},
+                {name: 'exmTm'},
+                {name: 'hmTypeNm'},
+                {name: 'hmKyNm'},
+                {name: 'hmDeptNm'},
+                {name: 'hallBldgNm'},
+                {name: 'hallNm'},
+                {name: 'scorerNm'},
+                {name: 'cancelMemo'},
+                {name: 'mblUid'},
+                {name: 'cancelSeq'},
+                {name: 'cancelDttm'}
+            ];
+
+            for (var i = 0; i < colModel.length; i++) {
+                colModel[i].label = colModel[i].label === undefined ? colModel[i].name : colModel[i].label;
+            }
+
+            var opt = $.extend(true, {
+                defaults: {
+                    url: 'status/major',
+                    colModel: colModel
+                }
+            }, options);
+
+            this.constructor.__super__.initialize.call(this, opt);
+        },
+        render: function () {
+            this.constructor.__super__.render.call(this);
+            return this;
+        }
+    });
+});
