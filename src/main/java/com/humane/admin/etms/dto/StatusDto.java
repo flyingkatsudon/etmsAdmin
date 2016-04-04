dfc24dbc925459da6644407f34e85990b1b3b36f
MuzEye <muzeye@gmail.com>
2016-04-04 19:59:35 +0900

    .

--- src/main/java/com/humane/admin/etms/dto/StatusDto.java
+++ src/main/java/com/humane/admin/etms/dto/StatusDto.java
@@ -2,7 +2,6 @@ package com.humane.admin.etms.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.databind.ser.std.TimeZoneSerializer;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
