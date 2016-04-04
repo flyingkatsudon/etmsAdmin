107a3695bcc97eb93c77eb251e4fada91f8f784c
MuzEye <muzeye@gmail.com>
2016-04-04 23:11:55 +0900

    .

--- src/main/java/com/humane/admin/etms/controller/ExportController.java
+++ src/main/java/com/humane/admin/etms/controller/ExportController.java
@@ -14,7 +14,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import rx.Observable;
+import rx.schedulers.Schedulers;
 
+import javax.servlet.AsyncContext;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -38,71 +41,86 @@ public class ExportController {
     public void attend(@RequestParam(value = "filters", required = false) String filters,
                        @RequestParam(value = "sidx", required = false) String sidx,
                        @RequestParam(value = "sord", required = false) String sord,
-                       HttpServletResponse response) throws IOException, DRException {
+                       HttpServletRequest request
+    ) throws IOException, DRException {
+        final AsyncContext async = request.startAsync();
+        async.start(() -> {
+            HttpServletResponse asyncRes = (HttpServletResponse) async.getResponse();
 
-        String query = JqgridMapper.getQueryString(filters);
-        String[] sort = JqgridMapper.getSortString(sidx, sord);
+            String query = JqgridMapper.getQueryString(filters);
+            String[] sort = JqgridMapper.getSortString(sidx, sord);
 
-        Observable.range(0, Integer.MAX_VALUE)
-                .concatMap(currentPage -> apiService.statusAttend(query, currentPage, Integer.MAX_VALUE, sort))
-                .takeUntil(pageResponse -> pageResponse.body().isLast())
-                .reduce(new ArrayList<>(), (list, pageResponse) -> {
-                    list.addAll(pageResponse.body().getContent());
-                    return list;
-                })
-                //.subscribeOn(Schedulers.computation())
-                //.observeOn(Schedulers.newThread())
-                .subscribe(list -> {
-                    JasperReportBuilder report = report()
-                            .columns(col.column("구분", "admissionNm", type.stringType()),
-                                    col.column("전형", "attendTypeNm", type.stringType()),
-                                    col.column("응시율", "attendPer", type.longType()),
-                                    col.column("지원자", "examineeCnt", type.longType()),
-                                    col.column("응시자", "attendCnt", type.longType()),
-                                    col.column("결시자", "absentCnt", type.longType()))
-                            .setDataSource(new JRBeanCollectionDataSource(list));
+            Observable.range(0, Integer.MAX_VALUE)
+                    .concatMap(currentPage -> apiService.statusAttend(query, currentPage, Integer.MAX_VALUE, sort))
+                    .takeUntil(pageResponse -> pageResponse.body().isLast())
+                    .reduce(new ArrayList<>(), (list, pageResponse) -> {
+                        list.addAll(pageResponse.body().getContent());
+                        return list;
+                    })
+                    .subscribeOn(Schedulers.computation())
+                    .observeOn(Schedulers.newThread())
+                    .subscribe(list -> {
+                        JasperReportBuilder report = report()
+                                .columns(col.column("구분", "admissionNm", type.stringType()),
+                                        col.column("전형", "attendTypeNm", type.stringType()),
+                                        col.column("지원자", "examineeCnt", type.longType()),
+                                        col.column("응시자", "attendCnt", type.longType()),
+                                        col.column("응시율", "attendPer", type.longType()),
+                                        col.column("결시자", "absentCnt", type.longType()),
+                                        col.column("결시율", "absentPer", type.longType())
+                                )
+                                .setDataSource(new JRBeanCollectionDataSource(list));
 
-                    toXlsx(report, response, "전형별 응시율");
-                });
+                        toXlsx(asyncRes, report, "전형별 응시율");
+                        async.complete();
+                    });
+        });
     }
 
     @RequestMapping("examinee")
     public void examinee(@RequestParam(value = "filters", required = false) String filters,
                          @RequestParam(value = "sidx", required = false) String sidx,
                          @RequestParam(value = "sord", required = false) String sord,
-                         HttpServletResponse response) throws IOException, DRException {
+                         HttpServletRequest request
+    ) throws IOException, DRException {
 
-        String query = JqgridMapper.getQueryString(filters);
-        String[] sort = JqgridMapper.getSortString(sidx, sord);
+        final AsyncContext async = request.startAsync();
+        async.start(() -> {
+            HttpServletResponse asyncRes = (HttpServletResponse) async.getResponse();
 
-        Observable.range(0, Integer.MAX_VALUE)
-                .concatMap(currentPage -> apiService.statusExaminee(query, currentPage, Integer.MAX_VALUE, sort))
-                .takeUntil(pageResponse -> pageResponse.body().isLast())
-                .reduce(new ArrayList<>(), (list, pageResponse) -> {
-                    list.addAll(pageResponse.body().getContent());
-                    return list;
-                })
-                //.subscribeOn(Schedulers.computation())
-                //.observeOn(Schedulers.newThread())
-                .subscribe(list -> {
-                    JasperReportBuilder report = report()
-                            .columns(
-                                    col.column("구분", "admissionNm", type.stringType()),
-                                    col.column("모집단위", "majorNm", type.stringType()),
-                                    col.column("전공", "deptNm", type.stringType()),
-                                    col.column("수험번호", "examineeCd", type.stringType()),
-                                    col.column("수험생", "examineeNm", type.stringType()),
-                                    col.column("시험일자", "attendDate", type.dateType()),
-                                    col.column("시험시간", "attendTime", type.timeHourToSecondType()),
-                                    col.column("응시여부", "isAttend", type.booleanType())
-                            )
-                            .setDataSource(new JRBeanCollectionDataSource(list));
+            String query = JqgridMapper.getQueryString(filters);
+            String[] sort = JqgridMapper.getSortString(sidx, sord);
 
-                    toXlsx(report, response, "수험생별 응시현황");
-                });
+            Observable.range(0, Integer.MAX_VALUE)
+                    .concatMap(currentPage -> apiService.statusExaminee(query, currentPage, Integer.MAX_VALUE, sort))
+                    .takeUntil(pageResponse -> pageResponse.body().isLast())
+                    .reduce(new ArrayList<>(), (list, pageResponse) -> {
+                        list.addAll(pageResponse.body().getContent());
+                        return list;
+                    })
+                    .subscribeOn(Schedulers.computation())
+                    .observeOn(Schedulers.newThread())
+                    .subscribe(list -> {
+                        JasperReportBuilder report = report()
+                                .columns(
+                                        col.column("구분", "admissionNm", type.stringType()),
+                                        col.column("모집단위", "majorNm", type.stringType()),
+                                        col.column("전공", "deptNm", type.stringType()),
+                                        col.column("수험번호", "examineeCd", type.stringType()),
+                                        col.column("수험생", "examineeNm", type.stringType()),
+                                        col.column("시험일자", "attendDate", type.dateType()),
+                                        col.column("시험시간", "attendTime", type.timeHourToSecondType()),
+                                        col.column("응시여부", "isAttend", type.booleanType())
+                                )
+                                .setDataSource(new JRBeanCollectionDataSource(list));
+
+                        toXlsx(asyncRes, report, "수험생별 응시현황");
+                        async.complete();
+                    });
+        });
     }
 
-    private void toXlsx(JasperReportBuilder report, HttpServletResponse response, String title) {
+    private void toXlsx(HttpServletResponse response, JasperReportBuilder report, String title) {
         report.ignorePageWidth()
                 .ignorePagination()
                 .setPageMargin(DynamicReports.margin(0))
