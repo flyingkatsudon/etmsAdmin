639b962fd3117f04738af7cff6d632557447002a
MuzEye <muzeye@gmail.com>
2016-04-11 10:43:33 +0900

    .

--- src/main/java/com/humane/admin/etms/dto/StatusDto.java
+++ src/main/java/com/humane/admin/etms/dto/StatusDto.java
@@ -11,7 +11,7 @@ import java.util.Date;
 
 @Data
 @NoArgsConstructor
-@JsonInclude(JsonInclude.Include.NON_EMPTY)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class StatusDto implements Serializable {
     String admissionNm;
     String attendNm;
