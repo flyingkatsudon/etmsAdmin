cd5df075f0ed9c7520bae5d332dd04a5ce66c413
MuzEye <muzeye@gmail.com>
2016-04-26 17:46:49 +0900

    .

--- src/main/java/com/humane/admin/etms/config/WebSecurityConfig.java
+++ src/main/java/com/humane/admin/etms/config/WebSecurityConfig.java
@@ -1,6 +1,5 @@
 package com.humane.admin.etms.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,16 +11,20 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/").permitAll()
+                .antMatchers("/", "/login").permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .formLogin().loginPage("/login").permitAll()
+                .formLogin()
+                .loginPage("/login")
+                .failureUrl("/login?error")
+                .defaultSuccessUrl("/main", true)
                 .and()
-                .logout().permitAll();
+                .logout()
+                .logoutSuccessUrl("/login?logout");
     }
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
                 .withUser("admin").password("humane12!").roles("USER");
     }
