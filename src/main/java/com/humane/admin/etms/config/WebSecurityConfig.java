72a63537991e21ad69ae6cfa0c36c1cc4ac02a3c
MuzEye <muzeye@gmail.com>
2016-05-03 10:20:35 +0900

    update bower

--- src/main/java/com/humane/admin/etms/config/WebSecurityConfig.java
+++ src/main/java/com/humane/admin/etms/config/WebSecurityConfig.java
@@ -13,7 +13,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         http.authorizeRequests()
-                .antMatchers("/", "/login", "/bower_components/**").permitAll()
+                .antMatchers("/", "/login", "/bower_components/**", "/dist/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
