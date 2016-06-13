07fb7b8e3ac82aa247dd2af6eda802e82b2f110c
MuzEye <muzeye@gmail.com>
2016-06-13 09:49:33 +0900

    .

--- src/main/java/com/humane/etms/model/Attend.java
+++ src/main/java/com/humane/etms/model/Attend.java
@@ -22,11 +22,11 @@ public class Attend {
     @Column(columnDefinition = "int default 5") private int paperLen;  // 답안지 자리수.
     @Column(columnDefinition = "bit default 1") private boolean isPaperChange;
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
     @Temporal(TemporalType.DATE)
     private Date attendDate; // 출결일자
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm:ss", timezone = "Asia/Seoul")
     @Temporal(TemporalType.TIME)
     private Date attendTime; // 출결시간
 }
