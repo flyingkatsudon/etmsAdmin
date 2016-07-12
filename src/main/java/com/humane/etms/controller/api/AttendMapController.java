88787d6c6cbedab83543fec1786c979ae8bb7aba
MuzEye <muzeye@gmail.com>
2016-07-12 11:30:20 +0900

    attendMap add mgr

--- src/main/java/com/humane/etms/controller/api/AttendMapController.java
+++ src/main/java/com/humane/etms/controller/api/AttendMapController.java
@@ -4,6 +4,7 @@ import com.humane.etms.model.AttendMap;
 import com.humane.etms.model.QAttendMap;
 import com.humane.etms.repository.AttendMapRepository;
 import com.humane.util.spring.data.JoinDescriptor;
+import com.mysema.query.BooleanBuilder;
 import com.mysema.query.types.Predicate;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,7 +40,37 @@ public class AttendMapController {
 
     @RequestMapping(method = RequestMethod.POST)
     public ResponseEntity<AttendMap> merge(@RequestBody AttendMap attendMap) {
-        AttendMap rtn = repository.save(attendMap);
+        // 채점자는 채점자의 내용만 손대야 한다.
+        QAttendMap qAttendMap = QAttendMap.attendMap;
+        AttendMap find = repository.findOne(new BooleanBuilder()
+                .and(qAttendMap.attend.eq(attendMap.getAttend()))
+                .and(qAttendMap.examinee.eq(attendMap.getExaminee()))
+                .and(qAttendMap.hall.eq(attendMap.getHall()))
+        );
+        AttendMap rtn = null;
+        if (find != null) {
+            attendMap.setIsNoIdCard(find.getIsNoIdCard());
+            rtn = repository.save(attendMap);
+        }
+
+        return new ResponseEntity<>(rtn, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "mgr", method = RequestMethod.POST)
+    public ResponseEntity<AttendMap> mgr(@RequestBody AttendMap attendMap) {
+        // 중간관리자는 중간관리자의 내용만 손대야한다.
+        QAttendMap qAttendMap = QAttendMap.attendMap;
+        AttendMap find = repository.findOne(new BooleanBuilder()
+                .and(qAttendMap.attend.eq(attendMap.getAttend()))
+                .and(qAttendMap.examinee.eq(attendMap.getExaminee()))
+                .and(qAttendMap.hall.eq(attendMap.getHall()))
+        );
+        AttendMap rtn = null;
+        if (find != null) {
+            find.setIsNoIdCard(attendMap.getIsNoIdCard());
+            rtn = repository.save(find);
+        }
+
         return new ResponseEntity<>(rtn, HttpStatus.OK);
     }
 
