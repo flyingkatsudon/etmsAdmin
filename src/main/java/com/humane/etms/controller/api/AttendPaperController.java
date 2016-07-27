d6f3f582ce026eb9a5f8077ec0d563397d9b92d9
MuzEye <muzeye@gmail.com>
2016-07-27 18:00:07 +0900

    .

--- src/main/java/com/humane/etms/controller/api/AttendPaperController.java
+++ src/main/java/com/humane/etms/controller/api/AttendPaperController.java
@@ -1,7 +1,9 @@
 package com.humane.etms.controller.api;
 
 import com.humane.etms.model.AttendPaper;
+import com.humane.etms.model.QAttendPaper;
 import com.humane.etms.repository.AttendPaperRepository;
+import com.mysema.query.BooleanBuilder;
 import com.mysema.query.types.Predicate;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +19,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "api/attendPaper", produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
@@ -30,13 +35,41 @@ public class AttendPaperController {
 
     @RequestMapping(method = RequestMethod.POST)
     public ResponseEntity<AttendPaper> merge(@RequestBody AttendPaper attendPaper) {
+        QAttendPaper qAttendPaper = QAttendPaper.attendPaper;
+
+        AttendPaper find = repository.findOne(new BooleanBuilder()
+                .and(qAttendPaper.attend.attendCd.eq(attendPaper.getAttend().getAttendCd()))
+                .and(qAttendPaper.paperCd.eq(attendPaper.getPaperCd()))
+        );
+
+        if (find != null) {
+            attendPaper.set_id(find.get_id());
+            if (find.getOldPaperCd() != null) attendPaper.setOldPaperCd(find.getOldPaperCd());
+        }
+
         AttendPaper rtn = repository.save(attendPaper);
         return new ResponseEntity<>(rtn, HttpStatus.OK);
     }
 
     @RequestMapping(value = "list", method = RequestMethod.POST)
     public ResponseEntity<Iterable<AttendPaper>> merge(@RequestBody Iterable<AttendPaper> attendPapers) {
-        Iterable<AttendPaper> rtn = repository.save(attendPapers);
-        return new ResponseEntity<>(rtn, HttpStatus.OK);
+        QAttendPaper qAttendPaper = QAttendPaper.attendPaper;
+
+        List<AttendPaper> list = new ArrayList<>();
+
+        attendPapers.forEach(attendPaper -> {
+            AttendPaper find = repository.findOne(new BooleanBuilder()
+                    .and(qAttendPaper.attend.attendCd.eq(attendPaper.getAttend().getAttendCd()))
+                    .and(qAttendPaper.paperCd.eq(attendPaper.getPaperCd()))
+            );
+
+            if (find != null) {
+                attendPaper.set_id(find.get_id());
+                if (find.getOldPaperCd() != null) attendPaper.setOldPaperCd(find.getOldPaperCd());
+            }
+
+            list.add(repository.save(attendPaper));
+        });
+        return new ResponseEntity<>(list, HttpStatus.OK);
     }
 }
