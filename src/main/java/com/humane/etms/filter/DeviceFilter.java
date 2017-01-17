8e1617406e1907105f034b034080080bdd9ae1f3
MuzEye <muzeye@gmail.com>
2017-01-17 11:58:10 +0900

    단말기가 아닐 경우 필터 추가.

--- src/main/java/com/humane/etms/filter/DeviceFilter.java
+++ src/main/java/com/humane/etms/filter/DeviceFilter.java
@@ -6,6 +6,7 @@ import com.humane.etms.repository.DeviceRepository;
 import com.humane.util.filter.RequestWrapper;
 import com.querydsl.core.BooleanBuilder;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -31,29 +32,31 @@ public class DeviceFilter extends GenericFilterBean {
 
         Map<String, String> headerMap = requestWrapper.headerMap();
 
-        String uuid = headerMap.get("uuid");
-        String deviceId = headerMap.get("deviceid");
-        String packageName = headerMap.get("packagename");
-        String phoneNo = headerMap.get("phoneno");
-        String versionName = headerMap.get("versionname");
+        String uuid = headerMap.get("uuid"); // 단말기 고유번호
+        String phoneNo = headerMap.get("phoneno"); // 전화번호. 없을수도 있다.
+        String deviceId = headerMap.get("deviceid"); // 단말기 등록번호 ex : 1
+        String packageName = headerMap.get("packagename"); // 앱 com.humane.etms2.app 등등.
+        String versionName = headerMap.get("versionname"); // 앱 버전
 
-        Device device = deviceRepository.findOne(new BooleanBuilder()
-                .and(QDevice.device.uuid.eq(uuid))
-                .and(QDevice.device.packageName.eq(packageName))
-        );
+        if (!StringUtils.isAnyEmpty(uuid, packageName)) {
+            Device device = deviceRepository.findOne(new BooleanBuilder()
+                    .and(QDevice.device.uuid.eq(uuid))
+                    .and(QDevice.device.packageName.eq(packageName))
+            );
 
-        if (device == null) {
-            device = new Device();
-            device.setUuid(uuid);
-            device.setPackageName(packageName);
+            if (device == null) {
+                device = new Device();
+                device.setUuid(uuid);
+                device.setPackageName(packageName);
+            }
+
+            device.setDeviceId(deviceId);
+            device.setPhoneNo(phoneNo);
+            device.setVersionName(versionName);
+            device.setLastDttm(new DateTime().toDate());
+
+            deviceRepository.save(device);
         }
-
-        device.setDeviceId(deviceId);
-        device.setPhoneNo(phoneNo);
-        device.setVersionName(versionName);
-        device.setLastDttm(new DateTime().toDate());
-
-        deviceRepository.save(device);
 
         chain.doFilter(request, response);
     }
