f7a30b5256f5e28b95e290f1d510f37096f32e80
Jeremy <milky0610.mk@gmail.com>
2017-07-17 11:56:00 +0900

    Merge branch 'master' into korea_2.0.0
    # Conflicts:
    #	src/main/resources/static/dist/css/sb-admin-2.css

--- src/main/java/com/humane/etms/model/AttendMap.java
+++ src/main/java/com/humane/etms/model/AttendMap.java
@@ -37,4 +37,7 @@ public class AttendMap implements Serializable {
     @Column(columnDefinition = "bit") private Boolean isScanner;
     private String groupOrder;
     private Long deviceId;
+
+    private String debateNm;
+    private String debateOrder;
 }
