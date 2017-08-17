a0af3f71c012f1511d2776020950fbd99783d64c
Jeremy <milky0610.mk@gmail.com>
2017-08-17 15:43:57 +0900

    기술요원 관리 -> 디자인 수정 및 예외처리 추가

--- src/main/java/com/humane/etms/controller/admin/SystemController.java
+++ src/main/java/com/humane/etms/controller/admin/SystemController.java
@@ -185,6 +185,7 @@ public class SystemController {
         try {
             Attend attend = attendRepository.findOne(new BooleanBuilder()
                     .and(QAttend.attend.admission.admissionNm.eq(param.getAdmissionNm()))
+                    .and(QAttend.attend.attendNm.eq(param.getAttendNm()))
                     .and(QAttend.attend.attendDate.eq(param.getAttendDate()))
                     .and(QAttend.attend.attendTime.eq(param.getAttendTime()))
             );
@@ -201,12 +202,13 @@ public class SystemController {
                     param.setAttendCd(attend.getAttendCd());
                     systemMapper.addStaff(param);
                 } else {
-                    return ResponseEntity.ok("이미 등록된 기술요원입니다");
+                    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("이미 등록된 기술요원입니다");
                 }
             }
             return ResponseEntity.ok("추가되었습니다");
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("빈 값을 확인하세요. 콤보박스의 값을 모두 특정하세요");
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("관리자에게 문의하세요<br><br>" + e.getMessage());
         }
     }
 
@@ -215,6 +217,7 @@ public class SystemController {
         try {
             Attend attend = attendRepository.findOne(new BooleanBuilder()
                     .and(QAttend.attend.admission.admissionNm.eq(param.getAdmissionNm()))
+                    .and(QAttend.attend.attendNm.eq(param.getAttendNm()))
                     .and(QAttend.attend.attendDate.eq(param.getAttendDate()))
                     .and(QAttend.attend.attendTime.eq(param.getAttendTime()))
             );
@@ -231,12 +234,12 @@ public class SystemController {
             if (tmp == null) {
                 systemMapper.modifyStaff(param);
             } else {
-                return ResponseEntity.ok("이미 등록된 기술요원입니다");
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("빈 값을 확인하세요. 콤보박스의 값을 모두 특정하세요");
             }
-
             return ResponseEntity.ok("변경되었습니다");
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("잠시 후 다시 시도하세요");
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("관리자에게 문의하세요<br><br>" + e.getMessage());
         }
     }
 
@@ -245,6 +248,7 @@ public class SystemController {
         try {
             Attend attend = attendRepository.findOne(new BooleanBuilder()
                     .and(QAttend.attend.admission.admissionNm.eq(param.get_admissionNm()))
+                    .and(QAttend.attend.attendNm.eq(param.getAttendNm()))
                     .and(QAttend.attend.attendDate.eq(param.get_attendDate()))
                     .and(QAttend.attend.attendTime.eq(param.get_attendTime()))
             );
@@ -254,7 +258,8 @@ public class SystemController {
             systemMapper.delStaff(param);
             return ResponseEntity.ok("삭제되었습니다");
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("잠시 후 다시 시도하세요");
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("관리자에게 문의하세요<br><br>" + e.getMessage());
         }
     }
 
@@ -264,7 +269,8 @@ public class SystemController {
             systemMapper.delStaffAll();
             return ResponseEntity.ok("삭제되었습니다");
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("잠시 후 다시 시도하세요");
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("관리자에게 문의하세요<br><br>" + e.getMessage());
         }
     }
 }
--- src/main/resources/static/js/grid/system-staff.js
+++ src/main/resources/static/js/grid/system-staff.js
@@ -15,6 +15,8 @@ define(function (require) {
             var colModel = [
                 {name: 'admissionCd', hidden: true},
                 {name: 'admissionNm', label: '전형명'},
+                {name: 'attendCd', hidden: true},
+                {name: 'attendNm', label: '시험명'},
                 {name: 'attendDate', label: '시험일자'},
                 {name: 'attendTime', label: '시험시간'},
                 {name: 'staffNm', label: '성명'},
@@ -45,47 +47,56 @@ define(function (require) {
                         var dialog = new BootstrapDialog({
                             title: '<h5><div id="alert" style="font-weight: bold; font-size: medium; color: crimson"><span style="color: black">기술요원 정보를 확인하세요</span></div></h5>',
                             message: html,
+                            closable: false,
                             onshown: function () {
-
                                 $('#staff').append(
-                                    '<div style="margin:1% 0 1% 3%; width:47%;">' + '성&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;명' +
-                                    '<input id="name" size="12" type="text" style="width: 60%; border-radius: 10px; padding: 1%; margin-left: 20%;" value="' + rowData.staffNm + '"/>' +
+                                    '<div style="margin: 1% 0 1% 3%;">' +
+                                        '<div style="float: left">성&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;명</div>' +
+                                        '<div style="width: 42%">' +
+                                            '<input id="name" size="12" type="text" style="border-radius: 10px; padding: 1%; margin-left: 22%;" value="' + rowData.staffNm + '"/>' +
+                                        '</div>' +
                                     '</div>'
                                 );
                                 $('#staff').append(
-                                    '<div style="margin:1% 0 1% 3%; width:47%; float:left">' + '전화번호' +
-                                    '<input id="first" size="4" type="text" style="border-radius: 10px; padding: 1%; margin-left: 20%" value="' + phone[0] + '">&nbsp;-&nbsp;' +
-                                    '<input id="middle" size="4" type="text" style="border-radius: 10px; padding: 1%;" value="' + phone[1] + '">&nbsp;-&nbsp;' +
-                                    '<input id="last" size="4" type="text" style="border-radius: 10px; padding: 1%;" value="' + phone[2] + '">' +
-                                    '</div>' +
-                                    '<div id="msg" style="margin:1% 0 1% 3%; width:47%; float:left; vertical-align: middle; color: crimson"></div>'
+                                    '<div style="margin:1% 0 1% 3%; width: 100%; float:left">' +
+                                        '<div style="float:left">전화번호</div>' +
+                                        '<div style="width: 42%; float:left">' +
+                                            '<input id="first" size="4" type="text" style="border-radius: 10px; padding: 1%; margin-left: 20%" value="' + phone[0] + '">&nbsp;-&nbsp;' +
+                                            '<input id="middle" size="4" type="text" style="border-radius: 10px; padding: 1%;" value="' + phone[1] + '">&nbsp;-&nbsp;' +
+                                            '<input id="last" size="4" type="text" style="border-radius: 10px; padding: 1%;" value="' + phone[2] + '">' +
+                                        '</div>' +
+                                    '<div id="msg" style="margin:1% 0 1% 3%; width:45%; float:left; vertical-align: middle; color: crimson"></div>' +
+                                    '</div>'
                                 );
-                                $('#staff').append(
+                                    $('#staff').append(
                                     '<div id="line" style="margin: 10% 0 5% 0; border: 1px solid #d8d6d5"></div>'
                                 );
                                 $('#staff').append(
-                                    '<div style="margin:1% 0 1% 1%; width:47%; float:left">' + '전&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;형' +
-                                    '<select id="staffAdm" style="width: 50%; margin-left: 20%; padding: 1%">' +
-                                    '</select>' +
+                                    '<div class="staff-toolbar">' + '전&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;형' +
+                                    '<select id="staffAdm" class="staff-select"></select>' +
                                     '</div>'
                                 );
                                 $('#staff').append(
-                                    '<div style="margin:1% 0 1% 1%; width:47%; float:left">' + '시험일자' +
-                                    '<select id="staffDate" style="width: 50%; margin-left: 20%; padding: 1%">' +
-                                    '</select>' +
+                                    '<div class="staff-toolbar">' + '시&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;험' +
+                                    '<select id="staffAtn" class="staff-select"></select>' +
                                     '</div>'
                                 );
                                 $('#staff').append(
-                                    '<div style="margin:1% 0 1% 1%; width:47%; float:left">' + '시험시간' +
-                                    '<select id="staffTime" style="width: 50%; margin-left: 20%; padding: 1%">' +
-                                    '</select>' +
+                                    '<div class="staff-toolbar">' + '시험일자' +
+                                    '<select id="staffDate" class="staff-select"></select>' +
                                     '</div>'
                                 );
                                 $('#staff').append(
-                                    '<div style="margin:1% 0 1% 1%; width:47%; float:left">' + '고사건물' +
-                                    '<select id="staffBldg" style="width: 50%; margin-left: 20%; padding: 1%">' +
-                                    '</select>' +
+                                    '<div class="staff-toolbar">' + '시험시간' +
+                                    '<select id="staffTime" class="staff-select"></div>'
+                                );
+                                $('#staff').append(
+                                    '<div class="staff-toolbar">' + '고사건물' +
+                                    '<select id="staffBldg" class="staff-select"></select>' +
                                     '</div>'
+                                );
+                                $('#staff').append(
+                                    '<div style="margin:2% 0 1% 3%; width:45%; float:left; vertical-align: middle; color: crimson"><span id="msg2"></span></div>'
                                 );
 
                                 // 기술요원 추가 dialog에 사용될 툴바
@@ -96,6 +107,7 @@ define(function (require) {
                                 }).render();
 
                                 $('#staffAdm').val(rowData.admissionNm).attr('selected', 'selected');
+                                $('#staffAtn').val(rowData.attendNm).attr('selected', 'selected');
                                 $('#staffDate').val(rowData.attendDate).attr('selected', 'selected');
                                 $('#staffTime').val(rowData.attendTime).attr('selected', 'selected');
                                 $('#staffBldg').val(rowData.bldgNm).attr('selected', 'selected');
@@ -109,19 +121,19 @@ define(function (require) {
                                     if (check_key(event) != 1) {
                                         $('#' + id).val('');
                                         //  event.returnValue = false;
-                                        $("#msg").html("숫자만 입력할 수 있습니다.");
+                                        $('#msg').html('숫자만 입력할 수 있습니다');
                                         return false;
                                     } else {
-                                        $("#msg").html("");
+                                        $('#msg').html('');
 
                                         if (id == 'first') {
                                             if (text.length > 3) {
-                                                $("#msg").html("3자리까지 입력 가능합니다");
+                                                $('#msg').html('3자리까지 입력 가능합니다');
                                                 return false;
                                             }
                                         } else {
                                             if (text.length > 4) {
-                                                $("#msg").html("4자리까지 입력 가능합니다");
+                                                $('#msg').html('4자리까지 입력 가능합니다');
                                                 return false;
                                             }
                                         }
@@ -162,6 +174,7 @@ define(function (require) {
                                             staffNm: $('#name').val(),
                                             phoneNo: $('#first').val() + '-' + $('#middle').val() + '-' + $('#last').val(),
                                             admissionNm: $('#staffAdm').val(),
+                                            attendNm: $('#staffAtn').val(),
                                             attendDate: $('#staffDate').val(),
                                             attendTime: $('#staffTime').val(),
                                             bldgNm: $('#staffBldg').val(),
@@ -172,6 +185,7 @@ define(function (require) {
                                             _phoneNo: rowData.phoneNo,
                                             _bldgNm: rowData.bldgNm,
                                             _admissionNm: rowData.admissionNm,
+                                            _attendNm: rowData.attendNm,
                                             _attendDate: rowData.attendDate,
                                             _attendTime: rowData.attendTime
                                         };
@@ -201,6 +215,7 @@ define(function (require) {
                                             $('#msg').html('성명을 입력하세요');
                                             $('#name').css('border', '3px solid crimson');
                                             $('#name').focus();
+                                            return false;
                                         }
 
                                         // 유효한 전화번호인지 검사
@@ -214,26 +229,38 @@ define(function (require) {
                                             $('#msg').html('3자리를 입력하세요');
                                             $('#first').css('border', '3px solid crimson');
                                             $('#first').focus();
+                                            return false;
                                         }
 
                                         if (middle.length != 4) {
                                             $('#msg').html('4자리를 입력하세요');
                                             $('#middle').css('border', '3px solid crimson');
                                             $('#middle').focus();
+                                            return false;
                                         }
 
                                         if (last.length != 4) {
                                             $('#msg').html('4자리를 입력하세요');
                                             $('#last').css('border', '3px solid crimson');
                                             $('#last').focus();
+                                            return false;
                                         }
 
-                                        $('msg').html('');
+                                        if ($('#staffAdm').val() == '' || $('#staffBldg').val() == '' ||
+                                            $('#staffAtn').val() == '' || $('#staffBldg').val() == '') {
+                                            $('#msg2').html('콤보박스의 값을 모두 특정하세요');
+                                            $('#msg2').fadeOut(100);
+                                            $('#msg2').fadeIn(100);
+                                            $('#msg2').fadeOut(100);
+                                            $('#msg2').fadeIn(100);
+                                            return false;
+                                        }
 
                                         var param = {
                                             staffNm: $('#name').val(),
                                             phoneNo: $('#first').val() + '-' + $('#middle').val() + '-' + $('#last').val(),
                                             admissionNm: $('#staffAdm').val(),
+                                            attendNm: $('#staffAtn').val(),
                                             attendDate: $('#staffDate').val(),
                                             attendTime: $('#staffTime').val(),
                                             bldgNm: $('#staffBldg').val(),
@@ -241,7 +268,8 @@ define(function (require) {
                                             // 기존 정보
                                             _staffNm: rowData.staffNm,
                                             _phoneNo: rowData.phoneNo,
-                                            _bldgNm: rowData.bldgNm
+                                            _bldgNm: rowData.bldgNm,
+                                            _attendCd: rowData.attendCd
                                         };
 
                                         $.ajax({
@@ -266,7 +294,7 @@ define(function (require) {
                         });
 
                         dialog.realize();
-                        dialog.getModalDialog().css('margin-top', '20%');
+                        dialog.getModalDialog().css('margin-top', '17%');
                         dialog.getModalDialog().css('width', '40%');
                         dialog.open();
                     }
